0dce0cbbbb92f6988964017598f2e4a10ba2c8fa
Andoly <andoly.cs@hotmail.com>
2021-08-13 21:28:42 -0300

    api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7cc509a51b51337ea49cbd8772650803'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2e367552-bf55-4fa9-9ec4-9370d738defc'
+};
